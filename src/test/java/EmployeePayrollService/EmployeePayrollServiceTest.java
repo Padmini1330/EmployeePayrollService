9ddd0dbbf9f6b982065d8a1515b05fe613859831
padmini v <padmini.v@perfios.com>
2021-09-19 18:46:31 +0530

    ADDED: test method that verifies the data written to file

---
+++ src/test/java/EmployeePayrollService/EmployeePayrollServiceTest.java
@@ -0,0 +1,27 @@
+package EmployeePayrollService;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import EmployeePayrollService.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest 
+{
+	@Test
+	public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries()
+	{
+		EmployeePayrollData[] arrayOfEmployees = {
+				new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+		};
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		
+	}
+}

17d526fc08dbaafb9e669b84399f155aea9ed26e
padmini v <padmini.v@perfios.com>
2021-09-19 16:12:17 +0530

    ADDED: class to maintain employee data and attributes

--- src/main/java/EmployeePayrollService/EmployeePayrollData.java
+++ src/main/java/EmployeePayrollService/EmployeePayrollData.java
@@ -2,6 +2,53 @@ package EmployeePayrollService;
 
 public class EmployeePayrollData 
 {
+	public int id;
+	public String name;
+	public double salary;
+	
+	public EmployeePayrollData(int id, String name, double salary) 
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() 
+	{
+		return id;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public double getSalary() 
+	{
+		return salary;
+	}
+
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public void setSalary(double salary) 
+	{
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
 	
 	
 }

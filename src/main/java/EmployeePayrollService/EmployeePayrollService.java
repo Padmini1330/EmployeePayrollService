5d13bf627714aeeed759aa0a926a6fa74c7c8aec
padmini v <padmini.v@perfios.com>
2021-09-19 15:58:10 +0530

    ADDED: Welcome message in main method

---
+++ src/main/java/EmployeePayrollService/EmployeePayrollService.java
@@ -0,0 +1,10 @@
+package EmployeePayrollService;
+
+public class EmployeePayrollService 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("***Welcome to Employee Payroll Service***");
+	}
+	
+}
